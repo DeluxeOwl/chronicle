b113db0df08d228fd5887869a450c656eae4a1f8
DeluxeOwl <nospamplease0@protonmail.com>
2025-05-30 18:58:06 +0300

    refactor: simplified metadata

--- message/message.go
+++ message/message.go
@@ -1,34 +1,12 @@
 package message
 
-import "maps"
-
 type Message interface {
 	Name() string
 }
 
 type Envelope[T Message] struct {
 	Message  T
-	Metadata Metadata
+	Metadata map[string]string
 }
 
 type GenericEnvelope Envelope[Message]
-
-type Metadata map[string]string
-
-func (m Metadata) With(key, value string) Metadata {
-	if m == nil {
-		m = make(Metadata)
-	}
-
-	m[key] = value
-
-	return m
-}
-
-func (m Metadata) Merge(other Metadata) Metadata {
-	if m == nil {
-		return other
-	}
-	maps.Copy(m, other)
-	return m
-}
