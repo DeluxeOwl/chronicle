b41000331fa320dd9dc557fb0ade902a9308138e
DeluxeOwl <nospamplease0@protonmail.com>
2025-06-03 14:12:04 +0300

    feat: added instance level check

--- aggregate/aggregate.go
+++ aggregate/aggregate.go
@@ -2,6 +2,8 @@ package aggregate
 
 import (
 	"fmt"
+	"reflect"
+	"sync"
 
 	"github.com/DeluxeOwl/eventuallynow/event"
 	"github.com/DeluxeOwl/eventuallynow/internal/registry"
@@ -43,10 +45,48 @@ type Root[TypeID ID] interface {
 
 type RegisterFunc func(eventName string, kind any)
 
+// Global map to track registration status per aggregate type.
+// The key is the reflect.Type of the aggregate (e.g., person.Person),
+// and the value is a sync.Once to ensure its RegisterEvents is called once.
+var (
+	typeRegistrations   = make(map[reflect.Type]*sync.Once)
+	typeRegistrationsMu sync.Mutex
+)
+
 func RecordEvent[TypeID ID](root Root[TypeID], e event.EventAny) error {
+	// Instance-level check: if this specific instance has already processed registration, skip.
+	// This is an optimization to avoid map lookups and sync.Once.Do checks on every event for an instance.
 	if !root.hasRegisteredEvents() {
-		root.RegisterEvents(registry.Register)
+		// Determine the concrete type of the aggregate root.
+		// We use .Elem() because 'root' is typically a pointer to a struct (e.g., *Person).
+		// We want the type of the struct itself (e.g., Person) as the key.
+		// Note: If 'root' could be a non-pointer struct that implements the interface,
+		// this might need adjustment, but typically aggregates are pointers.
+		var aggType reflect.Type
+		val := reflect.ValueOf(root)
+		if val.Kind() == reflect.Ptr {
+			aggType = val.Elem().Type()
+		} else {
+			aggType = val.Type() // Should not happen with current design if Root implies methods with ptr receivers
+		}
+
+		typeRegistrationsMu.Lock()
+		once, ok := typeRegistrations[aggType]
+		if !ok {
+			once = new(sync.Once)
+			typeRegistrations[aggType] = once
+		}
+		typeRegistrationsMu.Unlock()
+
+		// This will execute root.RegisterEvents(registry.Register) only once
+		// for the given aggType, across all instances and goroutines.
+		once.Do(func() {
+			root.RegisterEvents(registry.Register)
+		})
+
+		// Mark this specific instance as having gone through the registration process.
 		root.setRegisteredEvents()
 	}
+
 	return root.recordThat(root, event.New(e))
 }
--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -1,10 +1,22 @@
 package registry
 
-var eventFactories = make(map[string]func() any)
+import "sync"
 
-// TODO: concurrency safe
-// TODO: naming
+var (
+	eventFactories = make(map[string]func() any)
+	registryMu     sync.RWMutex // Mutex to protect eventFactories
+)
 
-func Register(eventName string, kind any) {
-	eventFactories[eventName] = func() any { return kind }
+func Register(eventName string, kindPrototype any) {
+	registryMu.Lock()
+	defer registryMu.Unlock()
+
+	eventFactories[eventName] = func() any { return kindPrototype }
+}
+
+func GetFactory(eventName string) (func() any, bool) {
+	registryMu.RLock()
+	defer registryMu.RUnlock()
+	factory, ok := eventFactories[eventName]
+	return factory, ok
 }
