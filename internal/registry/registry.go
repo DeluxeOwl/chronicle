015a9788c63c1b8eae638cb67b1681150de47045
DeluxeOwl <nospamplease0@protonmail.com>
2025-06-03 12:33:11 +0300

    chore: todos

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -4,6 +4,8 @@ var eventFactories = make(map[string]func() any)
 
 type EventRegistrar struct{}
 
+// TODO: concurrency safe
+// TODO: naming
 var Registrar = &EventRegistrar{}
 
 func (er *EventRegistrar) Register(eventName string, kind any) {
