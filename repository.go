3ab7503a826d5a578993aed67b80b3313173e572
DeluxeOwl <nospamplease0@protonmail.com>
2025-06-15 18:52:11 +0300

    feat: builder

--- repository.go
+++ repository.go
@@ -12,3 +12,13 @@ func NewEventSourcedRepository[TID aggregate.ID, E event.Any, R aggregate.Root[T
 ) (*aggregate.ESRepo[TID, E, R], error) {
 	return aggregate.NewESRepo(eventLog, newRoot, opts...)
 }
+
+func NewEventSourcedRepositoryWithSnapshots[TID aggregate.ID, E event.Any, R aggregate.Root[TID, E], TS aggregate.Snapshot[TID]](
+	eventLog event.Log,
+	newRoot func() R,
+	snapshotStore aggregate.SnapshotStore[TID, TS],
+	snapshotter aggregate.Snapshotter[TID, E, R, TS],
+	opts ...aggregate.ESRepoWithSnapshotsOption,
+) (*aggregate.ESRepoWithSnapshots[TID, E, R, TS], error) {
+	return aggregate.NewESRepoWithSnapshots(eventLog, newRoot, snapshotStore, snapshotter, opts...)
+}
