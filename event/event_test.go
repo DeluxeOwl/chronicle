5a0490df0cc34705515e41821b223929bc3a71af
DeluxeOwl <nospamplease0@protonmail.com>
2025-07-15 16:51:57 +0300

    feat: small test for any to concrete

---
+++ event/event_test.go
@@ -0,0 +1,16 @@
+package event_test
+
+import (
+	"testing"
+
+	"github.com/DeluxeOwl/chronicle/event"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEvent_AnyToConcrete(t *testing.T) {
+	t.Parallel()
+	e := new(fooEventAlpha)
+	concrete, ok := event.AnyToConcrete[FooEvent](e)
+	require.True(t, ok)
+	require.IsType(t, new(fooEventAlpha), concrete)
+}
