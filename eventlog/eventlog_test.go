e826effb7cee5e63c4d93a30d884e41c7c2eadf3
DeluxeOwl <nospamplease0@protonmail.com>
2025-08-12 13:30:34 +0300

    feat: postgres backend

--- eventlog/eventlog_test.go
+++ eventlog/eventlog_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/vfs"
+	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -18,7 +19,10 @@ import (
 	"github.com/DeluxeOwl/chronicle/eventlog"
 
 	"github.com/DeluxeOwl/chronicle/version"
+	_ "github.com/jackc/pgx/v5/stdlib" // Import the pgx stdlib driver
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 type eventLog struct {
@@ -29,6 +33,53 @@ type eventLog struct {
 type globalEventLog struct {
 	name string
 	log  event.GlobalLog
+}
+
+func setupPostgres(t *testing.T) (*sql.DB, func()) {
+	t.Helper()
+	ctx := t.Context()
+
+	user := "user"
+	password := "password"
+	dbName := "test-db"
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "postgres:16-alpine",
+			ExposedPorts: []string{"5432"},
+			WaitingFor:   wait.ForListeningPort(nat.Port("5432")),
+			Env: map[string]string{
+				"POSTGRES_DB":       dbName,
+				"POSTGRES_USER":     user,
+				"POSTGRES_PASSWORD": password,
+			},
+		},
+		Started: true,
+	})
+	require.NoError(t, err)
+
+	endpoint, err := container.Endpoint(ctx, "")
+	require.NoError(t, err)
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		user,
+		password,
+		endpoint,
+		dbName)
+
+	db, err := sql.Open("pgx", dsn)
+	require.NoError(t, err)
+
+	err = db.PingContext(ctx)
+	require.NoError(t, err)
+
+	cleanup := func() {
+		if err := container.Terminate(ctx); err != nil {
+			t.Logf("failed to terminate container: %s", err)
+		}
+	}
+
+	return db, cleanup
 }
 
 func setupEventLogs(t *testing.T) ([]eventLog, func()) {
@@ -52,6 +103,10 @@ func setupEventLogs(t *testing.T) ([]eventLog, func()) {
 	sqliteLog, err := eventlog.NewSqlite(sqliteDB)
 	require.NoError(t, err)
 
+	pg, cleanupPostgres := setupPostgres(t)
+	postgresLog, err := eventlog.NewPostgres(pg)
+	require.NoError(t, err)
+
 	return []eventLog{
 			{
 				name: "memory log",
@@ -65,12 +120,18 @@ func setupEventLogs(t *testing.T) ([]eventLog, func()) {
 				name: "sqlite log",
 				log:  sqliteLog,
 			},
+			{
+				name: "postgres log",
+				log:  postgresLog,
+			},
 		}, func() {
 			err := pebbleDB.Close()
 			require.NoError(t, err)
 
 			err = sqliteDB.Close()
 			require.NoError(t, err)
+
+			cleanupPostgres()
 		}
 }
 
@@ -95,6 +156,10 @@ func setupGlobalEventLogs(t *testing.T) ([]globalEventLog, func()) {
 	sqliteLog, err := eventlog.NewSqlite(sqliteDB)
 	require.NoError(t, err)
 
+	pg, cleanupPostgres := setupPostgres(t)
+	postgresLog, err := eventlog.NewPostgres(pg)
+	require.NoError(t, err)
+
 	return []globalEventLog{
 			{
 				name: "memory log",
@@ -108,12 +173,18 @@ func setupGlobalEventLogs(t *testing.T) ([]globalEventLog, func()) {
 				name: "sqlite log",
 				log:  sqliteLog,
 			},
+			{
+				name: "postgres log",
+				log:  postgresLog,
+			},
 		}, func() {
 			err := pebbleDB.Close()
 			require.NoError(t, err)
 
 			err = sqliteDB.Close()
 			require.NoError(t, err)
+
+			cleanupPostgres()
 		}
 }
 
--- eventlog/postgres.go
+++ eventlog/postgres.go
@@ -47,23 +47,34 @@ func PostgresTableName(tableName string) PostgresOption {
 			UNIQUE (log_id, version)
 		);`, tableName, dataType)
 
-		// Note PL/pgSQL function RAISE EXCEPTION includes the error message in the pq.Error.Detail field.
-		// The err.Error() contains this detail, which we parse.
-		// We use SELECT ... FOR UPDATE to lock the rows for the given log_id, preventing race conditions
-		// where another transaction could insert an event between our read (MAX(version)) and write (INSERT).
 		p.qCreateFunction = fmt.Sprintf(`
-        CREATE OR REPLACE FUNCTION chronicle_check_event_version()
-        RETURNS TRIGGER AS $$
-        DECLARE
-            max_version BIGINT;
-        BEGIN
-            SELECT COALESCE(MAX(version), 0) INTO max_version FROM %s WHERE log_id = NEW.log_id FOR UPDATE;
-            IF NEW.version != max_version + 1 THEN
-                RAISE EXCEPTION '%s%%', max_version;
-            END IF;
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;`, tableName, conflictErrorPrefix)
+		CREATE OR REPLACE FUNCTION chronicle_check_event_version()
+		RETURNS TRIGGER AS $$
+		DECLARE max_version BIGINT;
+		BEGIN
+			-- Acquire a transaction-level advisory lock based on a hash of the log_id.
+			-- This serializes inserts for the same log_id, preventing the race condition
+			-- where two transactions simultaneously try to insert the first event for a stream.
+			-- The lock is automatically released at the end of the transaction.
+			PERFORM pg_advisory_xact_lock(hashtext(NEW.log_id));
+
+			-- Now that we have the lock, we can safely check the current version.
+			SELECT version INTO max_version FROM %s WHERE log_id = NEW.log_id ORDER BY version DESC LIMIT 1;
+			
+			-- If no row was found (i.e., this is the first event for this log_id),
+			-- the 'max_version' will be NULL. COALESCE handles this, setting it to 0.
+			IF NOT FOUND THEN
+				max_version := 0;
+			END IF;
+
+			IF NEW.version != max_version + 1 THEN
+				-- Raise an exception with the actual latest version to be parsed by the client.
+				RAISE EXCEPTION '%s%%', max_version;
+			END IF;
+
+			RETURN NEW;
+		END;
+		$$ LANGUAGE plpgsql;`, tableName, conflictErrorPrefix)
 
 		p.qCreateTrigger = fmt.Sprintf(`
         DROP TRIGGER IF EXISTS trg_chronicle_check_event_version ON %s;
